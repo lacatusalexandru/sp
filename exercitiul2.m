631effcf549bbee13408a78c5b98d79df1294edf
lacatusalexandru <alexlaca96@gmail.com>
2016-10-29 21:09:16 +0300

    Update exercitiul2.m

--- exercitiul2.m
+++ exercitiul2.m
@@ -1,5 +1,9 @@
+%n=input('Introduceti rezolutia temporala in secunde = ');
+%t=0:n:5;
+%s=sawtooth(2*pi*t,1)
+%plot(t,s), xlabel('t'), ylabel('A'), title('TRIUNGHIULAR'), grid
+%axis([0 5 1 -2])
 n=input('Introduceti rezolutia temporala in secunde = ');
 t=0:n:5;
-s=sawtooth(2*pi*t,1)
-plot(t,s), xlabel('t'), ylabel('A'), title('TRIUNGHIULAR'), grid
-axis([0 5 1 -2])
+s=sawtooth(2*pi*t,0.5)
+plot(t,s), xlabel('t'), ylabel('A'), title('TRIUNGHIULAR'), grid on,axis([0 5 1 -2])
