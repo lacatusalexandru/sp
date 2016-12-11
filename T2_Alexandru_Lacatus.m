67fa7af759e6dd7017cc00a3d7ce37c480333001
lacatusalexandru <alexlaca96@gmail.com>
2016-12-11 18:47:53 +0200

    Create T2_Alexandru_Lacatus.m

---
+++ T2_Alexandru_Lacatus.m
@@ -0,0 +1,35 @@
+t = linspace(-50,50,100);
+f = 0;
+P = 40;
+N=50;
+omega = (2*pi)/P; 
+
+for i=-N:1:N
+    
+    if (i==0)
+        continue
+    else  
+    %C=((exp((-1)*1i*k*omega*P)-1)/(1i*k*omega*P));
+    %syms t1;
+    %C=int( exp((-1)*1i*k*omega*t1), t1, 0, P);
+    %fk = C*exp(omega*k*1i*t1);
+    %f = f + fk;
+    
+    C = ((1)/(pi*1i*i))*(1-exp(-pi*1i*i));    
+    fk = C*exp(w0*1i*i*t);                
+    f = f + fk;
+    
+    end
+end
+f=(1/P)*f;
+%drept = square(t1,50)
+s = sinc(pi*t2);
+
+subplot(2,1,1);
+plot(t, f, '-');
+%plot(drept, t1);
+grid on, xlabel('t'), ylabel('f(t)'), title('Reconstructia folosind coeficienti');
+
+subplot(2,1,2);
+stem(t,abs(s));
+grid on, xlabel('t'), ylabel('f(t)'), title('Spectrul lui x(t)');
