9c8b91148c892a17a389b4de90743445e632a0d3
lacatusalexandru <alexlaca96@gmail.com>
2016-12-11 23:30:04 +0200

    Update T2_Alexandru_Lacatus_424C.m

--- T2_Alexandru_Lacatus_424C.m
+++ T2_Alexandru_Lacatus_424C.m
@@ -6,6 +6,7 @@ omega = (2*pi)/P;
 
 for i=-N:1:N
     
+    %if isempty(i)
     if (i==0)
         continue
     else  
@@ -15,21 +16,22 @@ for i=-N:1:N
     %fk = C*exp(omega*k*1i*t1);
     %f = f + fk;
     
-    C = ((1)/(pi*1i*i))*(1-exp(-pi*1i*i));    
-    fk = C*exp(omega*1i*i*t);                
+    C = ((1)/(pi*1i*i))*(1-exp(-pi*1i*i)); %calculul coeficientului folosind integrala   
+    fk = C*exp(omega*1i*i*t);%coeficientul de ordinul i si suma lor
     f = f + fk;
     
     end
 end
 f=(1/P)*f;
-%drept = square(t1,50)
+%drept = square(t1,50);
 s = sinc(pi*t);
 
 subplot(2,1,1);
 plot(t, f, '-');
-%plot(drept, t1);
+%plot(drept, t1); // daca se incearca reprezentarea semnalului pe acelasi grafic apar erori
 grid on, xlabel('t'), ylabel('f(t)'), title('Reconstructia folosind coeficienti');
 
 subplot(2,1,2);
 stem(t,abs(s));
 grid on, xlabel('t'), ylabel('f(t)'), title('Spectrul lui x(t)');
+%in reprezentarea grafica se pot observa componenta continua, componenta fundamentala (i=1), celelalte componente fiind cele de ordinul i
